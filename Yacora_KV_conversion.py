27bdf6484e1acd17b5a72cf65ad6dd930a18fa71
NickOzzy <nick.osborne@ukaea.uk>
2021-08-09 00:04:49 +0100

    update from pc

--- Yacora_KV_conversion.py
+++ Yacora_KV_conversion.py
@@ -12,11 +12,11 @@ def TECPEC_Yacora(type, N, Ne, Tev, TiHmP, TiHpP):
 
 
     if type != 'HmHp' and type != 'HmH2p':
-        PECv = np.zeros(len(N), len(Ne))
+        PECv = np.zeros((len(N), len(Ne)))
         PECv[PECv == 0] = np.nan
-        for i in range(0,len(N)-1):
-            PECv[i][:] = interp2L(['nel'], data['Te'], np.squeeze(data['PEC'][:][:][N[i]]), Ne, Tev)
-        return
+        for i in range(0, len(N)):
+            PECv[i][:] = interp2L(data['nel'], data['Te'], np.squeeze(data['PEC'][:][:][N[i]]), Ne, Tev)
+        return PECv
 
     elif type == 'HmHp':
         TiHmP = TiHmP[:]*11600
@@ -27,8 +27,8 @@ def TECPEC_Yacora(type, N, Ne, Tev, TiHmP, TiHpP):
         AdjF = interp2d((data['TiHmP'], data['TiHpP']), data['AdjF'], (TiHmP, TiHpP))
         PECv = np.zeros(len(N), len(Ne))
         PECv[PECv == 0] = np.nan
-        for i in range(0,len(N-1)):
-            PECv[i][:] = AdjF * interp2L(['nel'], data['Te'], np.squeeze(data['PEC'][:][:][N[i]]), Ne, Tev)
+        for i in range(0,len(N)):
+            PECv[i][:] = AdjF * interp2L(data['nel'], data['Te'], np.squeeze(data['PEC'][:][:][N[i]]), Ne, Tev)
         return
 
     elif type == 'HmH2p':
@@ -36,12 +36,12 @@ def TECPEC_Yacora(type, N, Ne, Tev, TiHmP, TiHpP):
         TiHmP = TiHmP[:] * 11600
         TiHpP = TiHpP[:] * 11600
         TiHmP = np.clip(TiHmP, min(data['TiHm']), max(data['TiHm']))
-        TiHpP = np.clip(TiHpP, min(data['TiH2P']), max(data['TiH2P']))
+        TiHpP = np.clip(TiHpP, min(data['TiH2p']), max(data['TiH2p']))
 
-        PECv = np.zeros(len(N), len(Ne))
+        PECv = np.zeros((len(N), len(Ne)))
         PECv[PECv == 0] = np.nan
 
-        for i in range(0,len(N-1))
+        for i in range(0, len(N)):
             PECv[i][:] = interp4L(data['nel'], data['TiHm'], data['TiH2p'], data['Te'], np.squeeze(data['PEC'][:][:][:][:][N(i)]), Ne, TiHmP, TiHpP, Tev)
         return
 
@@ -53,3 +53,9 @@ def interp2L(x, y, V, xN, yN):
 def interp4L(x, y, x1, x2, V, xN, yN, xN1, xN2):
     point = np.array(np.log10(xN), np.log10(yN), np.log10(xN1), np.log10(xN2))
     return 10 ** interpn(np.array((np.log10(x), np.log10(y), np.log10(x1), np.log10(x2))), np.log10(V), point)
+
+print(TECPEC_Yacora('H2', [4], [2e19], [1], None, None))
+print(TECPEC_Yacora('H2p', [4], [2e19], [1], None, None))
+print(TECPEC_Yacora('H3p', [3], [2e19], [1], None, None))
+print(TECPEC_Yacora('HmH2p', [5], [2e19], [1], [2.2], [1]))
+#print(TECPEC_Yacora('HmHp', 5, 2e19, 1, 2.2, 1))
