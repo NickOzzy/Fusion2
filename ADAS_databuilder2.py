fee148217c7e9e77723a0f9693c1c0ebc0d256d1
NickOzzy <nick.osborne@ukaea.uk>
2021-08-31 23:22:11 +0100

    CheckButton for Adas and Yacora

--- ADAS_databuilder2.py
+++ ADAS_databuilder2.py
@@ -1,17 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, CheckButtons
 import matplotlib
-from scipy.interpolate import make_interp_spline
-
 matplotlib.use('TkAgg')
-
-
 
 
 def assemble(Density, no_ne):
     ratios_ADAS = {}
+    ratios_Yacora = {}
     for j, pair in enumerate(pairs):
         for k, T in enumerate(T_vals):
             APEC1 = ADAS_Recombination_PECs["n=" + str(j + 4) + "Den=" + Density + "T=" + str(T)].iloc[0]["PEC"]
@@ -19,43 +16,53 @@ def assemble(Density, no_ne):
             APEC3 = ADAS_Recombination_PECs["n=" + str(j + 3) + "Den=" + Density + "T=" + str(T)].iloc[0]["PEC"]
             APEC4 = ADAS_Excitation_PECs["n=" + str(j + 3) + "Den=" + Density + "T=" + str(T)].iloc[0]["PEC"]
             ratios_ADAS["pair="+pair+" T="+str(T)] = ((APEC1 + no_ne*APEC2)/(APEC3 + no_ne*APEC4))
-    return ratios_ADAS
+            YPEC1 = YDAS_Recombination_PECs["n=" + str(j + 4)+ "Den=" + Density + "T=" + str(T)].iloc[0]["PEC"]
+            YPEC2 = YDAS_Excitation_PECs["n=" + str(j + 4)+ "Den=" + Density + "T=" + str(T)].iloc[0]["PEC"]
+            YPEC3 = YDAS_Recombination_PECs["n=" + str(j + 3) + "Den=" + Density + "T=" + str(T)].iloc[0]["PEC"]
+            YPEC4 = YDAS_Excitation_PECs["n=" + str(j + 3) + "Den=" + Density + "T=" + str(T)].iloc[0]["PEC"]
+            ratios_Yacora["pair="+pair+"T="+ str(T)] = ((YPEC1 + no_ne*YPEC2)/(YPEC3 + no_ne*YPEC4))
+    return ratios_ADAS, ratios_Yacora
 
 
 def plotting():
         data = []
-        data_dict = assemble(density, no_ne)
+        data_dict_A, data_dict_Y = assemble(density, no_ne)
         graph = []
-        spline_graph = []
+        graph2 = []
 
         for i, pair in enumerate(pairs):
-            data.append({'Temp': T_vals, 'ratio': [val for key, val in data_dict.items() if pair in key]})
-            graph_frame = pd.DataFrame(data[i], columns=['Temp', 'ratio'])
+            data.append({'Temp': T_vals, 'ratio': [val for key, val in data_dict_A.items() if pair in key],
+                         'ratio2': [val for key, val in data_dict_Y.items() if pair in key]})
+            graph_frame = pd.DataFrame(data[i], columns=['Temp', 'ratio', 'ratio2'])
             x_axis = graph_frame['Temp'].tolist()
             y_axis = graph_frame['ratio'].tolist()
+            y_axis2 = graph_frame['ratio2'].tolist()
             plt.xscale("log")
-            graph.append(plt.plot(x_axis, y_axis, marker='o', color=colour_list[i], label=pair))
-            # x_new = np.logspace(0, 1.3, 50)
-            # splined = make_interp_spline(x_axis, y_axis, k=2)
-            # y_new = splined(x_new)
-            # spline_graph.append(plt.plot(x_new, y_new, color=colour_list[i]))
+            graph.append(plt.plot(x_axis, y_axis, marker='o', color=colour_list[i], label=pair, visible=False))
+            graph2.append(plt.plot(x_axis, y_axis2, color=colour_list[i], linestyle="dashed", visible=False))
         plt.ylim(0, 0.6)
         plt.xlabel("Temperature in eV")
         plt.ylabel("Balmer line ratio")
         plt.legend(loc="upper right")
-        return graph
+        return graph, graph2
 
 
 def update(val):
     data = []
     density = str('{:0.0e}'.format(10**(D_Slider.val), 0)).replace("+", "")
     no_ne = no_ne_Slider.val
-    data_dict = assemble(density, no_ne)
+    data_dict_A, data_dict_Y = assemble(density, no_ne)
+
     for i, pair in enumerate(pairs):
-        data.append({'Temp': T_vals, 'ratio': [val for key, val in data_dict.items() if pair in key]})
-        graph_frame = pd.DataFrame(data[i], columns=['Temp', 'ratio'])
+        data.append({'Temp': T_vals, 'ratio': [val for key, val in data_dict_A.items() if pair in key],
+                     'ratio2': [val for key, val in data_dict_Y.items() if pair in key]})
+        graph_frame = pd.DataFrame(data[i], columns=['Temp', 'ratio', 'ratio2'])
         y_axis = graph_frame['ratio'].tolist()
+        y_axis2 = graph_frame['ratio2'].tolist()
         graph[i][0].set_ydata(y_axis)
+        graph[i][0].set_visible(Y_radio.get_status()[1])
+        graph2[i][0].set_ydata(y_axis2)
+        graph2[i][0].set_visible(Y_radio.get_status()[0])
     D_Slider.valtext.set_text(density+" /m^3")
     fig.canvas.draw_idle()
 
@@ -73,6 +80,8 @@ if __name__ == '__main__':
 
     ADAS_Excitation_PECs = np.load("ADAS_Exc_PECs.npy", allow_pickle=True).item()
     ADAS_Recombination_PECs = np.load("ADAS_Rec_PECs.npy", allow_pickle=True).item()
+    YDAS_Excitation_PECs = np.load("Yacora_Exc_PECs.npy", allow_pickle=True).item()
+    YDAS_Recombination_PECs = np.load("Yacora_Rec_PECs.npy", allow_pickle=True).item()
     no_ne, density = 0.1, "2e19"
 
 
@@ -81,18 +90,24 @@ if __name__ == '__main__':
         pairs.append("B=" + str(n) + "/" + "B=" + str(n-1))
 
     fig = plt.figure()
-    plt.subplots_adjust(bottom=0.4)
-    graph = plotting()
-    D_axis = plt.axes([0.1, 0.25, 0.7, 0.02])
+    fig.set_size_inches(6,8)
+    plt.subplots_adjust(bottom=0.5)
+    graph, graph2 = plotting()
+    D_axis = plt.axes([0.1, 0.35, 0.7, 0.02])
     steps = [np.log10(5e18), np.log10(1e19), np.log10(2e19), np.log10(5e19), np.log10(1e20), np.log10(2e20)]
-    dmin, dmax, dinit = np.log10(5e18), np.log10(2e20), np.log10(5e19)
+    dmin, dmax, dinit = np.log10(5e18), np.log10(2e20), np.log10(2e19)
     D_Slider = Slider(D_axis, "density", dmin, dmax, valinit=dinit, valstep=steps)
     D_Slider.valtext.set_text(density+" /m^3")
     D_Slider.on_changed(update)
-    no_ne_axis = plt.axes([0.1, 0.15, 0.7, 0.02])
+    no_ne_axis = plt.axes([0.1, 0.4, 0.7, 0.02])
     no_nemin, no_nemax = 0, 1.1
     no_ne_Slider = Slider(no_ne_axis, "no/ne", no_nemin, no_nemax, valinit=0.1, valstep=np.arange(0, 1.11, 0.01))
     no_ne_Slider.on_changed(update)
+    Y_radio_axis = plt.axes([0.1, 0.1, 0.4, 0.2])
+    Y_radio = CheckButtons(Y_radio_axis, ['Yacora - no molecules', 'ADAS - no molecules', 'Yacora - include H2',
+                                          'Yacora - include H2+', 'Yacora - include H3+', 'Yacora - include H-H+',
+                                          'Yacora - include H-H2+'], [False]*7)
+    Y_radio.on_clicked(update)
     plt.show()
     # print(ADAS_Excitation_PECs["n=6Den=2e19T=7"].iloc[0]["PEC"])
     # print(ADAS_Excitation_PECs["n=3Den=5e19T=1"].iloc[0]["PEC"])
