3cec6e0bb37416ce0f8fd8cf928104cfcad7912a
NickOzzy <nick.osborne@ukaea.uk>
2021-09-23 18:01:47 +0100

    includes 1.5 Temp

--- new_Yac_data_generator.py
+++ new_Yac_data_generator.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 D_vals = ["5e18", "1e19", "2e19", "5e19", "1e20", "2e20"]
-T_vals = [1, 2, 3, 7, 10, 15, 20]
+T_vals = [1, 1.5, 2, 3, 7, 10, 15, 20]
 NIST_As = {"n=2": 4.6999e8, "n=3": 4.4114e7, "n=4": 8.4217e6, "n=5": 2.5311e6, "n=6": 9.7346e5, "n=7": 4.3901e5,
                "n=8": 2.3153e5, "n=9": 1.2159e5}  # data from NIST, note "n=2" is the lyman alpha coefficient.
 
@@ -10,6 +10,8 @@ Yacora_Rec_PEC_dict = {}
 
 for n in range(3,8,1):
     for T in T_vals:
+        if T == 1.5:
+            T = "1point5"
         for i, D in enumerate(D_vals):
             Yacora_Exc_PEC_dict["n="+str(n)+"Den="+D+"T="+str(T)] = np.genfromtxt("Data_files/ExcT"+str(T)+"n"+str(n)
                                                                                   +".dat", skip_header=27+i, skip_footer=5-i, usecols=(1))*NIST_As["n="+str(n)]
